586a5039ecac8fdc493ee83a24c35a51432a9f58
MacOS13 <macos13@MacBook-Pro-cua-MacOS13.local>
2019-02-23 10:22:06 +0700

    fix theme

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand navbar-dark bg-dark">
+<nav class="navbar navbar-expand navbar-dark nav-default">
     <a class="navbar-brand" href="https://cungcap.net" target="_blank"><img src="https://cungcap.net/themes/default/img/logo.svg" width="120"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
